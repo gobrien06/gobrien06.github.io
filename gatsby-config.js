014f6efcb81caeb9381737c9a584e0d1f61384db
gobrien06 <gobrien088@gmail.com>
2020-02-24 14:05:02 -0800

    Initial setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gabrielle O'Brien`,
+    description: `Fullstack engineer based in Irvine, California. Reimagining how we `,
+    author: `@gobrien06`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,7 +21,7 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#004CBB `,
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
--- src/components/header.js
+++ src/components/header.js
@@ -5,28 +5,74 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
+      background: '#FFFF'      ,
+      marginBottom: `1.3rem`,
+      boxShadow: '0 0 2vh #5555',
     }}
   >
+    <hr style={{ 
+      background: '#2F78D5',
+      height:'.4rem',
+    }} />
     <div
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        margin: `0`,
+        padding: `0.5rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 style={{ marginLeft: `1.55vh`, display:`inline`, fontWeight:`500`,}}>
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `black`,
             textDecoration: `none`,
           }}
         >
           {siteTitle}
         </Link>
-      </h1>
+        </h1>
+
+        <h3 style={{ float:`right`, marginRight:`1.5vh`, display:`inline`, fontWeight:`400`,}}>
+        <Link
+          to="/goals"
+          style={{
+            color: `black`,
+            textDecoration: `none`,
+            marginLeft:`1.5vh`,
+          }}
+        >
+          Goals
+        </Link>
+        <Link
+          to="/projects"
+          style={{
+            color: `black`,
+            textDecoration: `none`,
+          }}
+        >
+          Projects
+        </Link>
+        <Link
+          to="/designs"
+          style={{
+            color: `black`,
+            textDecoration: `none`,
+            marginLeft:`1.5vh`,
+          }}
+        >
+          Designs
+        </Link>
+        <Link
+          to="/github"
+          style={{
+            color: `black`,
+            textDecoration: `none`,
+            marginLeft:`1.5vh`,
+          }}
+        >
+          GitHub
+        </Link>
+      </h3>
     </div>
   </header>
 )
